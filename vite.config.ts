62cf5f845508b594b93fe96bbe3975a1059f4657
nurdin najib <najibnurdin999@gmail.com>
2024-12-17 10:29:42 +0700

    add exclude into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
        * Include 'example-dep' in the array below.
        * @see https://vitejs.dev/config/dep-optimization-options
        */
+      exclude: ['eval']
     },
   },
 });
